26317e609a444ff6c64247bba1ee668dcbeefb75
Victor <vcarvalho0402@gmail.com>
2022-08-09 18:10:03 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 
 app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
 
